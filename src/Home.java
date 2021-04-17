151e4ff672b5cc57deaff70327b57514c45565d3
Mrs'n <72780745+IlIlIlIlIlIlIll@users.noreply.github.com>
2021-04-17 10:49:28 +0400

    Modification de l'interface: - Ajout du titre "Departement des Ventes" - Ajout du boutton Nouvelle commande - Ajout du boutton Tous les commandes - Ajout du contentPanel ou l'on va deposer l'image pour l'interface

--- src/Home.java
+++ src/Home.java
@@ -7,6 +7,11 @@ import javax.swing.border.EmptyBorder;
 import java.awt.Button;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import java.awt.Color;
+import java.awt.Font;
 
 public class Home extends JFrame {
 
@@ -33,18 +38,40 @@ public class Home extends JFrame {
 	 */
 	public Home() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 614, 300);
+		setBounds(100, 100, 614, 394);
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		
-		Button button = new Button("Connexion");
+		Button button = new Button("Tous les commandes");
+		button.setBounds(342, 242, 204, 36);
 		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		contentPane.add(button, BorderLayout.SOUTH);
+		contentPane.setLayout(null);
+		contentPane.add(button);
+		
+		Button button_1 = new Button("Nouvelle commande");
+		button_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		button_1.setBounds(342, 180, 204, 36);
+		contentPane.add(button_1);
+		
+		JLabel lblNewLabel = new JLabel("Departement des Ventes");
+		lblNewLabel.setFont(new Font("Arial", Font.PLAIN, 14));
+		lblNewLabel.setBounds(362, 27, 204, 38);
+		contentPane.add(lblNewLabel);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.DARK_GRAY);
+		panel.setBounds(0, 0, 270, 355);
+		contentPane.add(panel);
+		
+		JLabel lblNewLabel_1 = new JLabel("");
+		panel.add(lblNewLabel_1);
 	}
-
 }
