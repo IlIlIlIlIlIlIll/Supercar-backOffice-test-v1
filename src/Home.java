3bc7b745d53bf20dbaef70e479b6df0df211148c
Mrs'n <72780745+IlIlIlIlIlIlIll@users.noreply.github.com>
2021-05-04 12:27:19 +0400

    Debug

--- src/Home.java
+++ src/Home.java
@@ -1,4 +1,3 @@
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -7,14 +6,17 @@ import javax.swing.border.EmptyBorder;
 import java.awt.Button;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import javax.swing.JButton;
-import javax.swing.JTextField;
 import javax.swing.JLabel;
 import java.awt.Color;
 import java.awt.Font;
+import javax.swing.ImageIcon;
 
 public class Home extends JFrame {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
 	/**
@@ -25,6 +27,7 @@ public class Home extends JFrame {
 			public void run() {
 				try {
 					Home frame = new Home();
+					frame.setUndecorated(true);			
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -38,14 +41,15 @@ public class Home extends JFrame {
 	 */
 	public Home() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 614, 394);
+		setBounds(100, 100, 751, 503);
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		
 		Button button = new Button("Tous les commandes");
-		button.setBounds(342, 242, 204, 36);
+		button.setFont(new Font("Arial", Font.BOLD, 13));
+		button.setBounds(461, 298, 204, 36);
 		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
@@ -54,24 +58,26 @@ public class Home extends JFrame {
 		contentPane.add(button);
 		
 		Button button_1 = new Button("Nouvelle commande");
+		button_1.setFont(new Font("Arial", Font.BOLD, 13));
 		button_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		button_1.setBounds(342, 180, 204, 36);
+		button_1.setBounds(461, 217, 204, 36);
 		contentPane.add(button_1);
 		
 		JLabel lblNewLabel = new JLabel("Departement des Ventes");
-		lblNewLabel.setFont(new Font("Arial", Font.PLAIN, 14));
-		lblNewLabel.setBounds(362, 27, 204, 38);
+		lblNewLabel.setFont(new Font("Arial", Font.BOLD | Font.ITALIC, 16));
+		lblNewLabel.setBounds(461, 30, 204, 38);
 		contentPane.add(lblNewLabel);
 		
 		JPanel panel = new JPanel();
 		panel.setBackground(Color.DARK_GRAY);
-		panel.setBounds(0, 0, 270, 355);
+		panel.setBounds(0, 0, 352, 464);
 		contentPane.add(panel);
 		
 		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setIcon(new ImageIcon("C:\\Users\\Developer\\eclipse-workspace\\Supercar\\src\\images\\9b64bd69f7f52ac64846c26625b7c3c3.jpg"));
 		panel.add(lblNewLabel_1);
 	}
 }
